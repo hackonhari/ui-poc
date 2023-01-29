f0d4237e113f0f1508046a205ef3468a8739a645
hackonhari <hackonhari@gmail.com>
2023-01-29 08:29:55 +0530

    feat: :sparkles: Update the button type and add eslint config

--- Button.tsx
+++ Button.tsx
@@ -1,4 +1,14 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
-};
+import React, { ReactElement } from "react";
+
+interface Props {
+  onClick: () => void;
+  label: string;
+}
+
+export default function Button({ onClick, label }: Props): ReactElement {
+  return (
+    <button onClick={onClick} type="submit">
+      {label}
+    </button>
+  );
+}
